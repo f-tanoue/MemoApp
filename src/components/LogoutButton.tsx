3c10c3b048b5ce6dd1ca66a1ba00dcf43c846cdb
f-tanoue <fuka-tanoue@froide.co.jp>
2024-09-16 15:51:17 +0900

    ログインを実装

--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -1,8 +1,22 @@
-import { Text, StyleSheet, TouchableOpacity } from "react-native";
+import { Text, StyleSheet, TouchableOpacity, Alert } from "react-native";
+import { signOut } from "firebase/auth";
+import { auth } from "../config";
+import { router } from "expo-router";
+
+const handlePress = () => {
+  //ログアウトの処理
+  signOut(auth)
+    .then(() => {
+      router.replace("/auth/log_in");
+    })
+    .catch(() => {
+      Alert.alert("ログアウトに失敗しました。");
+    });
+};
 
 const LogOutButton = () => {
   return (
-    <TouchableOpacity>
+    <TouchableOpacity onPress={handlePress}>
       <Text style={styles.text}>ログアウト</Text>
     </TouchableOpacity>
   );
