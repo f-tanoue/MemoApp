0f8c7742b901994e299b53586f980d0d32952559
f-tanoue <fuka-tanoue@froide.co.jp>
2024-09-16 16:31:34 +0900

    メモの新規作成とキーボードのバグ修正

--- src/app/memo/create.tsx
+++ src/app/memo/create.tsx
@@ -1,24 +1,43 @@
-import {
-  View,
-  TextInput,
-  StyleSheet,
-  KeyboardAvoidingView,
-} from "react-native";
+import { View, TextInput, StyleSheet } from "react-native";
 import CircleButton from "../../components/CircleButton";
 import Icon from "../../components/icon";
 import { router } from "expo-router";
+import { collection, addDoc, Timestamp } from "firebase/firestore";
+import { db, auth } from "../../config";
+import { useState } from "react";
+import KeyboardAvoidingView from "../../components/KeyboardAvoidingView";
 
-const handlePress = () => {
-  router.back();
+const handlePress = async (bodyText: string) => {
+  if (auth.currentUser === null) return;
+  const ref = collection(db, `users/${auth.currentUser.uid}/memos`);
+  //メモを作成
+  addDoc(ref, {
+    bodyText,
+    updateAt: Timestamp.fromDate(new Date()),
+  })
+    .then((docRef) => {
+      console.log("success", docRef.id);
+      router.back();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 const Create = () => {
+  const [bodyText, setBodyText] = useState("");
   return (
-    <KeyboardAvoidingView behavior="height" style={styles.container}>
+    <KeyboardAvoidingView style={styles.container}>
       <View style={styles.inputContainer}>
-        <TextInput multiline value="" style={styles.input} />
+        <TextInput
+          multiline
+          value={bodyText}
+          style={styles.input}
+          onChangeText={(text) => setBodyText(text)}
+          autoFocus
+        />
       </View>
-      <CircleButton onPress={handlePress}>
+      <CircleButton onPress={() => handlePress(bodyText)}>
         <Icon name="check" size={40} color="#FFFFFF" />
       </CircleButton>
     </KeyboardAvoidingView>
